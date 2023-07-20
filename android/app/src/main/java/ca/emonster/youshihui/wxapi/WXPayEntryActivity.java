f4745eed4e20c428f5c58ce79d034e35b8171ae0
Patrick Xu <82446720+patrickxunuo@users.noreply.github.com>
2023-07-20 10:09:25 -0700

    first commit

---
+++ android/app/src/main/java/ca/emonster/youshihui/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,14 @@
+package ca.emonster.youshihui.wxapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.wechatlib.WeChatLibModule;
+
+public class WXEntryActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    WeChatLibModule.handleIntent(getIntent());
+    finish();
+  }
+}
